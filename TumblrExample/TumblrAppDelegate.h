eb53d710331dab5e7a937b3885a7649a40abb708
msg-ios <msg.maruko@gmail.com>
2013-04-24 19:57:23 -0300

    Initial Commit

---
+++ TumblrExample/TumblrAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TumblrAppDelegate.h
+//  TumblrExample
+//
+//  Created by Marco S. Graciano on 4/24/13.
+//  Copyright (c) 2013 Marco Graciano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TumblrAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TumblrExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TumblrExample
+//
+//  Created by Marco S. Graciano on 4/24/13.
+//  Copyright (c) 2013 Marco Graciano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TumblrAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TumblrAppDelegate class]));
+    }
+}
